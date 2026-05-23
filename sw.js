aeb27e1082d9f90640ddf2778e9b8c9cefecf7d2
airlie <ajm201@cam.ac.uk>
2026-05-23 21:57:19 +0100

    Rename icon to icon.png

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'liking-likelihood-v2';
+const ASSETS = [
+  './',
+  './liking_likelihood.html',
+  './manifest.json',
+  './icon.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const networkFetch = fetch(event.request)
+        .then((resp) => {
+          if (resp && resp.status === 200 && resp.type === 'basic') {
+            const copy = resp.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          }
+          return resp;
+        })
+        .catch(() => cached);
+      return cached || networkFetch;
+    })
+  );
+});
